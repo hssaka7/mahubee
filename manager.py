e04b8ebc2eb0bcae39f8631753ccf5f079f1d4a6
hssaka7 <hssaka7@gmail.com>
2024-05-03 00:25:54 -0500

    added manager flow

--- manager.py
+++ manager.py
@@ -13,22 +13,28 @@ WORKSPACE = '/Users/aakashbasnet/development/python/workspace/ml_pipelines'
 
 # manager run 
 class Manager:
-    def __init__(self, steps):
+    def __init__(self, steps, workspace_path):
         id = uuid.uuid4()
         self.steps = steps
-
-        # create id folder for the pipleline
-        
-        pass
+        self.workspace = f"{workspace_path}/{id}"
+        create_workspace_folder(self.workspace)
     
+    def run(self):
+        results = dict()
+        for step in self.steps:
+            step_workspace = f"{self.workspace}/{step.name}"
+            create_workspace_folder(step_workspace, delete = True)
+            print(step.name)
+            step.run()
+            # save the returned results
+            # set the input for the next object
+
+
 def get_steps(p_config):
-     # TODO create dependency graph
+    
+    # TODO create dependency graph
     step_objs = []
     for step in p_config:
-        print(step['name'])
-        print(step['depends'])
-        print(step['class_name'])
-        print("======================")
 
         _folder, _file, _step = step['class_name'].split('.')
         mod = __import__(f"core.{_folder}.{_file}")
@@ -37,16 +43,14 @@ def get_steps(p_config):
         mod = getattr(mod, _step)
         obj = mod(**step)
         step_objs.append(obj)
-
+    return step_objs
 
 def start():
     # TODO setup loggers
 
     args = vars(get_argsparser().parse_args())
     pipeline_config= parse_config(f"core/{args['pipeline_config']}")
-    
-    print (args)
-    print(pipeline_config)
+ 
     
     # create pipeline folder in workspace
     pipeline_name = pipeline_config['pipeline_name']
@@ -55,8 +59,8 @@ def start():
 
     steps = get_steps(pipeline_config['steps'])
     
-    manager = Manager(steps)
-   
+    manager = Manager(steps , workspace_path)
+    manager.run()
         
     
 
